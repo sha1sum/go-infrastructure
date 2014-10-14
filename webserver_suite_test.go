92370e802169041552689ed2d24d3d7049973c90
Aaron Greenlee <aarongreenlee@gmail.com>
2014-10-14 12:04:51 -0400

    IN-1 - Setup Webserver Test Suite Runner

---
+++ webserver_suite_test.go
@@ -0,0 +1,13 @@
+package webserver_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Webserver Suite")
+}
